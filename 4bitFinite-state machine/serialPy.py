cff5553bd775719c974c332109641709b5bca1d5
Qinyuan Liu <fengziyuan72@gmail.com>
2022-09-14 21:36:02 +0100

    V0.0.5 FinitePyAPIWorking

--- 4bitFinite-state machine/serialPy.py
+++ 4bitFinite-state machine/serialPy.py
@@ -1,15 +1,59 @@
 # Importing Libraries
+from msilib.schema import Class
 import serial
 import time
 
 arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+
+
 def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
+    arduino.write(bytes(x, 'ASCII'))
     time.sleep(0.05)
     data = arduino.readline()
     return data
 
+
 while True:
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the value
+    for i in [2, 3, 4, 5]:
+        num = "${}".format(i)
+        num = num.rstrip()
+        value = write_read(num)
+        print(value)  # printing the value
+        time.sleep(0.2)
+
+    for i in [2, 3, 4, 5]:
+        num = "^{}".format(i)
+        num = num.rstrip()
+        value = write_read(num)
+        print(value)  # printing the value
+        time.sleep(0.2)
+
+
+class UNO_Serial:
+    def UNO_Serial():
+        arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+
+    def write_read(x):
+        arduino.write(bytes(x, 'ASCII'))
+        time.sleep(0.05)
+        data = arduino.readline()
+        return data
+
+    def test():
+        while True:
+        for i in [2, 3, 4, 5]:
+            num = "${}".format(i)
+            num = num.rstrip()
+            value = write_read(num)
+            print(value)  # printing the value
+            time.sleep(0.2)
+
+        for i in [2, 3, 4, 5]:
+            num = "^{}".format(i)
+            num = num.rstrip()
+            value = write_read(num)
+            print(value)  # printing the value
+            time.sleep(0.2)
+
+class fouritFinite:
+    
